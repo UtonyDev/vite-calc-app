ed82c1c8cb1973dbb4a84ad3c44470614616c038
UTony <udoyeanthony948@gmail.com>
2024-09-18 12:41:27 +0100

    sixth commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-calc-app/',
-})
+  base: '/vite-calc-app/', // Replace with your GitHub Pages repository name
+});
